7e617dd43b4fb0ca92ac21dcd5e97a669bd0613c
Patrik Eriksson <74836180+Tiramisioux@users.noreply.github.com>
2026-02-06 20:34:51 +0100

    Quiet alsaloop monitor output

--- cinepi/cinepi_sound.cpp
+++ cinepi/cinepi_sound.cpp
@@ -375,7 +375,9 @@ void CinePISound::startMonitoring() {
     std::ostringstream mon_cmd;
     mon_cmd << "alsaloop -C " << defaultDevice
             << " -P " << outputDevice
-            << " -t 10000 -A 1 -d";
+            << " -t 10000 -A 1 -d"
+            << " -q"
+            << " 2>/dev/null";
     console->info("Starting audio monitoring: {}", mon_cmd.str());
     monitor_pipe = popen2(mon_cmd.str(), "r", monitor_pid);
     if (monitor_pid > 0 && monitor_pipe) {
