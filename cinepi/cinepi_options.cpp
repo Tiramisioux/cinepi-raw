f567cfc2d8625542ba9aef00a6157df007a31f62
Patrik Eriksson <74836180+Tiramisioux@users.noreply.github.com>
2026-05-21 20:42:37 +0200

    Silence CinePi option logs for list-cameras

--- cinepi/cinepi_options.cpp
+++ cinepi/cinepi_options.cpp
@@ -488,6 +488,8 @@ bool CinePiOptions::Parse(int argc, char *argv[])
         /* Base class parses all remaining (rpicam-apps) options              */
         int new_argc = static_cast<int>(forward.size());
         bool ok = RawOptions::Parse(new_argc, forward.data());
+        if (!ok)
+                return false;
 
         /* --------------------------------------------------------------
          * Post-process scaler-crops string into vector<array<float,4>>
@@ -534,8 +536,7 @@ bool CinePiOptions::Parse(int argc, char *argv[])
         RawOptions::camPort = camPort;
 
         /* Log summary ------------------------------------------------- */
-        spdlog::info("cinepi-cli: camPort='{}'  hdmi_port={}  same_hdmi={}  zoom={}  crops={}"
-                     "crops={} rectangles",
+        spdlog::info("cinepi-cli: camPort='{}'  hdmi_port={}  same_hdmi={}  zoom={}  crops={} rectangles",
                      camPort,
                      hdmi_port,
                      same_hdmi ? "true" : "false",
